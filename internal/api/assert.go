a3ee66e8065ad1599196e6fd8b80cc363064fdc8
Filip Milichovsky <31818576+fmilichovsky@users.noreply.github.com>
2023-03-13 11:22:25 +0100

    DS-4908: Use testing interface in API assertions (#119)

--- internal/api/assert.go
+++ internal/api/assert.go
@@ -3,25 +3,26 @@ package api
 import (
 	"fmt"
 	"net/http"
-	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/portworx/pds-integration-test/internal/tests"
 )
 
-func NoError(t *testing.T, resp *http.Response, err error) bool {
+func NoError(t tests.T, resp *http.Response, err error) bool {
 	t.Helper()
 	apierr := ExtractErrorDetails(resp, err)
 	return assert.NoError(t, apierr, "Error calling PDS API.")
 }
 
-func NoErrorf(t *testing.T, resp *http.Response, err error, msg string, args ...any) bool {
+func NoErrorf(t tests.T, resp *http.Response, err error, msg string, args ...any) bool {
 	t.Helper()
 	apierr := ExtractErrorDetails(resp, err)
 	details := fmt.Sprintf(msg, args...)
 	return assert.NoErrorf(t, apierr, "Error calling PDS API: %s", details)
 }
 
-func RequireNoError(t *testing.T, resp *http.Response, err error) {
+func RequireNoError(t tests.T, resp *http.Response, err error) {
 	t.Helper()
 	if NoError(t, resp, err) {
 		return
@@ -29,7 +30,7 @@ func RequireNoError(t *testing.T, resp *http.Response, err error) {
 	t.FailNow()
 }
 
-func RequireNoErrorf(t *testing.T, resp *http.Response, err error, msg string, args ...any) {
+func RequireNoErrorf(t tests.T, resp *http.Response, err error, msg string, args ...any) {
 	t.Helper()
 	if NoErrorf(t, resp, err, msg, args...) {
 		return
